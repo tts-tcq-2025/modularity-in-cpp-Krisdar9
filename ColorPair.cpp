c1c098ca3249b98b37f969034e1f2058ea5939ff
Krisdar9 <104294585+Krisdar9@users.noreply.github.com>
2025-08-14 09:47:37 +0530

    Update ColorPair.cpp

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -13,7 +13,6 @@ namespace TelCoColorCoder
     MajorColor ColorPair::getMajor() {
         return majorColor;
     }
-
     MinorColor ColorPair::getMinor() {
         return minorColor;
     }
@@ -21,14 +20,12 @@ namespace TelCoColorCoder
     std::string ColorPair::ToString() {
         return std::string(MajorColorNames[majorColor]) + " " + MinorColorNames[minorColor];
     }
-
     ColorPair GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         MajorColor major = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
         MinorColor minor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
         return ColorPair(major, minor);
     }
-
     int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
         return major * numberOfMinorColors + minor + 1;
     }
